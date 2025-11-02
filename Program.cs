1d8b35d27a326a17b31e4f80066b10db1d4fe81b
Iryna <kytsenkoiryna33@gmail.com>
2025-11-02 22:14:17 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Bouquet
+{
+    public virtual void Show() => Console.WriteLine("Простий букетdotnet");
+}
+
+class WrappedBouquet : Bouquet
+{
+    private Bouquet bouquet;
+    public WrappedBouquet(Bouquet b) { bouquet = b; }
+
+    public override void Show()
+    {
+        bouquet.Show();
+        Console.WriteLine("Додано пакування");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Bouquet b = new Bouquet();
+        Bouquet wrapped = new WrappedBouquet(b);
+        wrapped.Show();
+    }
+}
+
+
+
+
+
+
+
+
